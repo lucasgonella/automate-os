1547ad991037881fca0af453dbcef2e04c0260d5
Lucas Gonella <goonella@hotmail.com>
2021-10-19 14:08:56 -0300

    Create bloquear-hosts.sh

---
+++ bloquear-hosts.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#Bloquear sites no hosts 
+
+#1º Passo
+#Entrar modo root
+
+#2º Passo
+
+echo -e "0.0.0.1 facebook.com\n0.0.0.1 pt-br.facebook.com\n0.0.0.1 www.facebook.com\n0.0.0.1 orkut.com\n0.0.0.1 ww.orkut.com\n0.0.0.1 tumblr.com\n0.0.0.1 www.tumblr.com\n0.0.0.1 ebuddy.com\n0.0.0.1 www.ebuddy.com\n0.0.0.1 weheartit.com\n0.0.0.1 www.weheartit.com\n0.0.0.1 http://weheartit.com\n0.0.0.1 ask.fm\n0.0.0.1 www.ask.fm\n0.0.0.1 http://ask.fm\n0.0.0.1 twitter.com\n0.0.0.1 www.twitter.com\n0.0.0.1 www.instagram.com\n0.0.0.1 instagram.com\n0.0.0.1 web.whatsapp.com" >> /etc/hosts
